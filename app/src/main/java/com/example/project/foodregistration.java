d185927bdd98d4c7fc1d9c674a17454db40e47ed
Yejean Kim <yejean1903@gmail.com>
2023-09-27 08:56:14 +0900

    first commit

---
+++ app/src/main/java/com/example/project/foodregistration.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class foodregistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.foodregistration);
+    }
+}
---
+++ app/src/main/java/com/example/project/searchpage.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class searchpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchpage);
+    }
+}
